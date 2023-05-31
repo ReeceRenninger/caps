750423aed7ee5def648f29edacfafaa586a325a1
ReeceRenninger <renningerreece@gmail.com>
2023-05-30 19:27:00 -0700

    tests started

--- driver/driver-handler.test.js
+++ driver/driver-handler.test.js
@@ -1,9 +1,7 @@
 'use strict';
 
-const { describe } = require('yargs');
 let eventEmitter = require('../eventPool');
 const { pickupOccured, packageDelivered } = require('./handler');
-const { test } = require('node:test');
 
 jest.mock('../eventPool.js', () => {
   return {
@@ -15,7 +13,23 @@ jest.mock('../eventPool.js', () => {
 console.log = jest.fn();
 
 describe('Testing driver handlers', () => {
-  test('log and emit in-transit', () => {
 
+  test('Should log and emit in-transit after pick up occurs', () => {
+    let payload = { orderId: 12345 };
+    pickupOccured(payload);
+
+    expect(eventEmitter.emit).toHaveBeenCalledWith('in-transit', payload);
+    expect(console.log).toHaveBeenCalledWith('DRIVER: picked up', payload.orderId);
   });
+
+
+  test('should emit delivered and log Driver delivery ', () => {
+    let payload = { orderId: 12345};
+    packageDelivered(payload);
+
+    expect(eventEmitter.emit).toHaveBeenCalledWith('delivered', payload);
+    expect(console.log).toHaveBeenCalledWith('DRIVER: delivered', payload.orderId);
+  });
+
+
 });
