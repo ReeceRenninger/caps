d7be1a2358a68aae0ba0fb9ca5f2a38d4e328cc5
ReeceRenninger <renningerreece@gmail.com>
2023-05-31 17:54:55 -0700

    all functionality and tests working

--- server/index.js
+++ server/index.js
@@ -1,8 +1,7 @@
 'use strict';
+//example to emit to room
+// socket.to(room).emit('some event', some-payload);
 // cheat sheet for socket connection https://socket.io/docs/v3/emit-cheatsheet/
-// server.emit sends to all clients connected to server
-//socket.broadcast.emit sends to all clients in current namespace except sender.
-// become the new hub? based off the file structure for lab.
 
 require('dotenv').config();
 const { Server } = require('socket.io');
@@ -10,7 +9,7 @@ const PORT = process.env.PORT || 3002;
 
 //create socket singleton
 const server = new Server();
-//namespace example
+//namespace creation
 const caps = server.of('/caps');
 
 function logger(event, payload){
@@ -22,13 +21,6 @@ function logger(event, payload){
 server.on('connection', (socket) => {
   console.log('Server socket connection to event server: ', socket.id);
 
-  //socket.on('looking for this message', (payload) => {
-  // console.log('SERVER: Message event', payload);
-  //})
-});
-
-server.on('MESSAGE', () => {
-  console.log('SERVER: Message event');
 });
 
 // connecting to caps namespace
@@ -61,6 +53,3 @@ caps.on('connection', (socket) => {
 
 //listening for all events at port 3001
 server.listen(PORT);
-
-//example to emit to room
-// socket.to(room).emit('some event', some-payload);
